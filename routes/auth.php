ccaf391769b2e5d8fe89703eec3e3a8ff75ab77b
ms-hamid <hamid.albase@gmail.com>
2025-05-13 01:18:13 +0700

    Route Website

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
